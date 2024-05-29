f85d29483c1660cddd8f3fd527c2dfe35565485c
solodkaLohyna <ira.palamarchuk.0110@gmail.com>
2024-05-30 01:53:49 +0300

    Add new paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./global.css";
-import App from "../app/App.tsx";
-import {Quiz}  from "./components/Quiz/Quiz.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "../app/App.tsx";
+import { Quiz } from "./components/Quiz/Quiz.tsx";
 import { NotFoundPage } from "./components/NotFoundPage/NotFoundPage.tsx";
+import { RegistrationForm } from "./components/RegistrationForm/RegistrationForm.tsx";
+import "./global.css";
+import { LoginForm } from "./components/LoginForm/LoginForm.tsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +19,14 @@ const router = createBrowserRouter([
   {
     path: '/quiz/:id',
     element: <Quiz />
+  },
+  {
+    path: '/registration',
+    element: <RegistrationForm />
+  },
+  {
+    path: '/login',
+    element: <LoginForm />
   }
 ]);
 
